3047bdc857c499d50687a5fe54007008b31920e8
Zebadiah Blackwell <zeb.blackwell@gmail.com>
2020-10-29 20:36:49 +0000

    Automatically backed up by Learn

---
+++ greeting.rb
@@ -0,0 +1,12 @@
+
+def greeting(name = "neighbor")
+  puts "Hello #{name}"
+end
+name = "Maria"
+greeting()
+greeting("dagd")
+
+def greeting_p(name,language)
+  puts "Hello #{name}. You like #{language}."
+end
+x = "Marasd"
---
+++ learning.rb
@@ -0,0 +1,5 @@
+count = 4
+count.times do
+  puts "Count is #{count}"
+  
+end
